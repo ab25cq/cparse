2eb454a48cf005d6767aa95c128897aec7b540cf
ab25cq <ab25cq@icloud.com>
2025-08-25 09:12:41 +0900

    iii# Please enter the commit message for your changes. Lines starting

--- fast_build.sh
+++ fast_build.sh
@@ -2,7 +2,8 @@ make clean
 make || exit 1
 
 # Parse all test C files in sorted order
-#for f in test/*.c; do
-#  [ -f "$f" ] || continue
-#  ./cparse "$f" || exit 3
-#done
+for f in test/*.c; do
+  [ -f "$f" ] || continue
+  echo $f
+  ./cparse "$f" || exit 3
+done
